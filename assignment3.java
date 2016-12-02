39e5b979b36c58f64b63eb2579f80a2be267c829
Angelo <aolcese1@jhu.edu>
2016-12-02 12:08:15 -0500

    Asa

--- assignment3.java
+++ assignment3.java
@@ -113,7 +113,7 @@ public class assignment3 {
                 JsonObject obj = Json.createObjectBuilder().add("recipient", "bob").add("messageID", "0").add("message", output).build();
                 String objString = obj.toString();
         
-                composeMessage(serverURL, port, "a", "bob", objString);
+                composeMessage(serverURL, port, "alice", "bob", objString);
             //}
         System.out.println(getAllMessages(serverURL, port, "a"));
             
