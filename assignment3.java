2098f0cef636cf3b516098d07028574d04fd71bc
Angelo <aolcese1@jhu.edu>
2016-12-05 12:39:03 -0500

    fucko

--- assignment3.java
+++ assignment3.java
@@ -148,7 +148,7 @@ public class assignment3 {
             val = ((byte)Integer.parseInt(number));
             cipherPad[16-i] = val;
             byte value = (byte)((byte)c2[c2.length-i] ^ ((byte)val));
-            //System.out.println("This plaintext byte value is: " + value);
+            System.out.println("This plaintext byte value is: " + value);
             /*for (int j = 1; j <= i; j++) {
                 String neededVal = String.valueOf((byte) cipherPad[16-j] ^ ((byte)(i+1))); 
                 data = recreateMaul(keys, data, serverURL, port, username, neededVal, c2.length - j);
